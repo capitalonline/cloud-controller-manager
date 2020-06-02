9113625011de0eec993b84a3c1d53b91e8140f2f
Honglin Feng <tifayuki@gmail.com>
2020-06-02 14:50:55 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/capitalonline/cloud-controller-manager
+
+go 1.14
---
+++ pkg/ccm/cloud.go
@@ -0,0 +1,6 @@
+package ccm
+
+const (
+	// ProviderName specifies the name for the DigitalOcean provider
+	ProviderName string = "cds"
+)
