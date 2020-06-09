f59255e2b5ddf7da27e030c956ed4c0bad319b1b
jizhi.sui@capitalonline.net <jizhi.sui@capitalonline.net>
2020-06-09 16:34:30 +0800

    fix bug1

--- pkg/ccm/resources.go
+++ pkg/ccm/resources.go
@@ -45,7 +45,24 @@ type syncer interface {
 type tickerSyncer struct{}
 
 func (s *tickerSyncer) Sync(name string, period time.Duration, stopCh <-chan struct{}, fn func() error) {
-	return
+	ticker := time.NewTicker(period)
+	defer ticker.Stop()
+
+	// manually call to avoid initial tick delay
+	if err := fn(); err != nil {
+		klog.Errorf("%s failed: %s", name, err)
+	}
+
+	for {
+		select {
+		case <-ticker.C:
+			if err := fn(); err != nil {
+				klog.Errorf("%s failed: %s", name, err)
+			}
+		case <-stopCh:
+			return
+		}
+	}
 }
 
 
@@ -79,7 +96,6 @@ func (r *ResourcesController) Run(stopCh <-chan struct{}) {
 		klog.Info("No cluster ID configured -- skipping cluster dependent syncers.")
 		return
 	}
-	// go r.syncer.Sync("tags syncer", controllerSyncTagsPeriod, stopCh, r.syncTags)
 }
 
 
