9719b630e3050103ca40f742228e053caca0b2f7
jizhi.sui@capitalonline.net <jizhi.sui@capitalonline.net>
2020-06-09 11:46:08 +0800

    change to digitalocean

--- cmd/main.go
+++ cmd/main.go
@@ -1,25 +1,46 @@
 package main
 
 import (
-	"flag"
 	"fmt"
+	"github.com/capitalonline/cloud-controller-manager/pkg/ccm"
 	"os"
-
-
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/pflag"
-
-	cliFlag "k8s.io/component-base/cli/flag"
 	"k8s.io/component-base/logs"
-	_ "k8s.io/component-base/metrics/prometheus/clientgo" // load all the prometheus client-go plugins
-	_ "k8s.io/component-base/metrics/prometheus/version"  // for version metric registration
 	"k8s.io/kubernetes/cmd/cloud-controller-manager/app"
 )
 
 func main() {
 	command := app.NewCloudControllerManagerCommand()
 
-	pflag.CommandLine.SetNormalizeFunc(cliFlag.WordSepNormalizeFunc)
-	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
+	// Set static flags for which we know the values.
+	command.Flags().VisitAll(func(fl *pflag.Flag) {
+		var err error
+		log.Infof("main:: fl.Name is: %s", fl.Name)
+		switch fl.Name {
+		case "allow-untagged-cloud",
+			// Untagged clouds must be enabled explicitly as they were once marked
+			// deprecated. See
+			// https://github.com/kubernetes/cloud-provider/issues/12 for an ongoing
+			// discussion on whether that is to be changed or not.
+			"authentication-skip-lookup":
+			// Prevent reaching out to an authentication-related ConfigMap that
+			// we do not need, and thus do not intend to create RBAC permissions
+			// for. See also
+			// https://github.com/digitalocean/digitalocean-cloud-controller-manager/issues/217
+			// and https://github.com/kubernetes/cloud-provider/issues/29.
+			err = fl.Value.Set("true")
+		case "cloud-provider":
+			// Specify the name we register our own cloud provider implementation
+			// for.
+			err = fl.Value.Set(ccm.ProviderName)
+		}
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "failed to set flag %q: %s\n", fl.Name, err)
+			os.Exit(1)
+		}
+	})
+
 	logs.InitLogs()
 	defer logs.FlushLogs()
 
