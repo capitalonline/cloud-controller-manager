5b67b8ab342f7fef0cfca13a164aebe8c63f36d2
jizhi.sui@capitalonline.net <jizhi.sui@capitalonline.net>
2020-06-16 18:15:59 +0800

    add unit test

---
+++ pkg/ccm/instance_test.go
@@ -0,0 +1,39 @@
+package ccm
+
+import (
+	"strings"
+	"testing"
+)
+
+func Test_getNodeInstanceTypeAndNodeNameByProviderID (t *testing.T) {
+
+	// params
+	fakeClusterID := ""
+	fakeProviderID := ""
+
+	// expected value
+	expectedNodeName := ""
+
+	// func
+	res, err := getNodeInstanceTypeAndNodeNameByProviderID(fakeClusterID, fakeProviderID)
+
+	// verify nodeName
+	actualNodeName := res.Data.NodeName
+	if strings.Compare(actualNodeName, expectedNodeName) != 0 {
+		t.Errorf("unexpected NodeName got: %s want: %s", actualNodeName, expectedNodeName)
+	}
+
+	// verify labels
+	actualLabels := res.Data.Labels
+	t.Logf("actualLabels is: %s", actualLabels)
+
+	// verify taints
+	actualTaints := res.Data.Taints
+	t.Logf("actualTaints is: %s", actualTaints)
+
+	// err
+	if err != nil {
+		t.Errorf("unexpected err, expected nil. got: %v", err)
+	}
+
+}
