261ae778bbf93eaa8469db43ba6c04b00d2f4fcd
jizhi.sui@capitalonline.net <jizhi.sui@capitalonline.net>
2020-08-13 14:46:51 +0800

    update vendor

--- pkg/ccm/log.go
+++ pkg/ccm/log.go
@@ -2,18 +2,14 @@ package ccm
 
 import (
 	"github.com/getsentry/sentry-go"
-	"log"
 
 	"time"
 )
 
 func SentrySendError(errorInfo error) {
 	// init by ENVIRONMNET
-	err := sentry.Init(sentry.ClientOptions{
+	_ = sentry.Init(sentry.ClientOptions{
 	})
-	if err != nil {
-		log.Fatalf("sentry.Init: %s", err)
-	}
 	// Flush buffered events before the program terminates.
 	defer sentry.Flush(2 * time.Second)
 
