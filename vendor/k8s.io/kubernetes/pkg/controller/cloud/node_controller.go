9f289f9d6644ece258665f3e30df7a37fad26c90
zbh173 <zbh173@163.com>
2023-01-07 14:46:54 +0800

    Update node_controller.go

--- vendor/k8s.io/kubernetes/pkg/controller/cloud/node_controller.go
+++ vendor/k8s.io/kubernetes/pkg/controller/cloud/node_controller.go
@@ -474,7 +474,7 @@ func ensureNodeExistsByProviderID(ctx context.Context, instances cloudprovider.I
 }
 
 func getNodeAddressesByProviderIDOrName(ctx context.Context, instances cloudprovider.Instances, node *v1.Node) ([]v1.NodeAddress, error) {
-	nodeAddresses, err := instances.NodeAddressesByProviderID(ctx, node.Spec.ProviderID, types.NodeName(node.Name))
+	nodeAddresses, err := instances.NodeAddressesByProviderID(ctx, node.Spec.ProviderID, node.Name)
 	if err != nil {
 		providerIDErr := err
 		nodeAddresses, err = instances.NodeAddresses(ctx, types.NodeName(node.Name))
